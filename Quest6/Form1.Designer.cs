39c87f99ac1408c4a5023806bc84493c1aa0c375
jnprogrammer <joshuanparker@gmail.com>
2017-09-09 19:15:53 -0400

    First Commit
    Made sure code worked and tweaked game to be a bit
    hard.

---
+++ Quest6/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Quest6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnFeed = new System.Windows.Forms.Button();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnListen = new System.Windows.Forms.Button();
+            this.btnTrick = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.barHunger = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOutPut = new System.Windows.Forms.RichTextBox();
+            this.barBoardem = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFeed
+            // 
+            this.btnFeed.Location = new System.Drawing.Point(14, 56);
+            this.btnFeed.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFeed.Name = "btnFeed";
+            this.btnFeed.Size = new System.Drawing.Size(108, 55);
+            this.btnFeed.TabIndex = 0;
+            this.btnFeed.Text = "Feed";
+            this.btnFeed.UseVisualStyleBackColor = true;
+            this.btnFeed.Click += new System.EventHandler(this.btnFeed_Click);
+            // 
+            // lblOut
+            // 
+            this.lblOut.AutoSize = true;
+            this.lblOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut.Location = new System.Drawing.Point(7, 11);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(132, 38);
+            this.lblOut.TabIndex = 1;
+            this.lblOut.Text = "Actions";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(14, 119);
+            this.btnListen.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(108, 55);
+            this.btnListen.TabIndex = 2;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // btnTrick
+            // 
+            this.btnTrick.Location = new System.Drawing.Point(14, 246);
+            this.btnTrick.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnTrick.Name = "btnTrick";
+            this.btnTrick.Size = new System.Drawing.Size(108, 55);
+            this.btnTrick.TabIndex = 3;
+            this.btnTrick.Text = "Do a Trick";
+            this.btnTrick.UseVisualStyleBackColor = true;
+            this.btnTrick.Click += new System.EventHandler(this.btnTrick_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(14, 182);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(108, 55);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(542, 8);
+            this.btn_save.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(108, 41);
+            this.btn_save.TabIndex = 5;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(403, 56);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 252);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // barHunger
+            // 
+            this.barHunger.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.barHunger.Location = new System.Drawing.Point(244, 56);
+            this.barHunger.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.barHunger.Maximum = 200;
+            this.barHunger.Name = "barHunger";
+            this.barHunger.Size = new System.Drawing.Size(144, 55);
+            this.barHunger.Step = 1;
+            this.barHunger.TabIndex = 7;
+            this.barHunger.Value = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Fullness ";
+            // 
+            // txtOutPut
+            // 
+            this.txtOutPut.Location = new System.Drawing.Point(158, 182);
+            this.txtOutPut.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtOutPut.Name = "txtOutPut";
+            this.txtOutPut.ReadOnly = true;
+            this.txtOutPut.Size = new System.Drawing.Size(232, 119);
+            this.txtOutPut.TabIndex = 9;
+            this.txtOutPut.Text = "";
+            // 
+            // barBoardem
+            // 
+            this.barBoardem.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.barBoardem.Location = new System.Drawing.Point(246, 119);
+            this.barBoardem.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.barBoardem.Maximum = 200;
+            this.barBoardem.Name = "barBoardem";
+            this.barBoardem.Size = new System.Drawing.Size(144, 55);
+            this.barBoardem.Step = 1;
+            this.barBoardem.TabIndex = 10;
+            this.barBoardem.Value = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(128, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Boardom";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 318);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.barBoardem);
+            this.Controls.Add(this.txtOutPut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.barHunger);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnTrick);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.btnFeed);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "crazy Critter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFeed;
+        private System.Windows.Forms.Label lblOut;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.Button btnTrick;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar barHunger;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtOutPut;
+        private System.Windows.Forms.ProgressBar barBoardem;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Quest6/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Quest6
+{
+    public partial class Form1 : Form
+    {
+        Critter thisCreature = new Critter("TeReck", 200, 0);
+        int foodBurn = 10, foodAmt = 10, boardRate = 10;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            barHunger.Maximum = 200;
+            barBoardem.Maximum = 100;
+            barBoardem.Value = thisCreature.Boredom;
+            barHunger.Value = thisCreature.Hunger;
+        }
+
+        private void btnFeed_Click(object sender, EventArgs e)
+        {
+            if (barHunger.Value == barHunger.Maximum)
+            {
+                btnFeed.Enabled = false;
+                txtOutPut.Text = txtOutPut.Text + "Not hungry don't feed me \n";
+            }
+            else
+            {
+                thisCreature.feed(foodAmt);
+                barHunger.Increment(thisCreature.Hunger);
+                txtOutPut.Text = txtOutPut.Text + "You fed it 10 pellets\n";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Decrease the creatures hunger and bar value so they are equally decremented 
+            barHunger.Increment(-foodBurn);
+            thisCreature.Hunger -= foodBurn;
+            barBoardem.Increment(boardRate);
+            thisCreature.Boredom += boardRate;
+           
+            // This checks the bar value
+        #region hunger
+            if (barHunger.Value < 200)
+            {
+                btnFeed.Enabled = true;
+            }
+            if(barHunger.Value == 150)
+            {
+                txtOutPut.Text = txtOutPut.Text + "I'm feeling for a snack\n";
+            }
+            if (barHunger.Value == 100)
+            {
+                txtOutPut.Text = txtOutPut.Text + "When's Dinner?\n";
+            }
+            if (barHunger.Value == 70)
+            {
+                txtOutPut.Text = txtOutPut.Text + "Hey I'm seriously hungry!!\n";
+            }
+            if (barHunger.Value == 50)
+            {
+                txtOutPut.Text = txtOutPut.Text + "Where is MY DINNER YOU MEAT THING?!!\n";
+            }
+            if (barHunger.Value == 20)
+            {
+                txtOutPut.Text = txtOutPut.Text + ". . .\n";
+            }
+            if (barHunger.Value == 0)
+            {
+                txtOutPut.Text = txtOutPut.Text + "YOU'RE MY DINNER !!! " + thisCreature.Name + " reveals its calws and sinks them into your legs\n and begins to rip into your tourso as you lose consciousness . . .\n all but a dream\n";
+                btnFeed.Enabled = false;
+                btnListen.Enabled = false;
+                btnPlay.Enabled = false;
+                btnTrick.Enabled = false;
+                barBoardem.Value = barBoardem.Minimum;
+                timer1.Stop();   
+            }
+            #endregion
+        #region boardom
+            if (thisCreature.Boredom == 30)
+            {
+                txtOutPut.Text = txtOutPut.Text + "Mood: Meh\n";
+            }
+            if (thisCreature.Boredom == 50)
+            {
+                txtOutPut.Text = txtOutPut.Text + "Wow nothing again?\n";
+            }
+            if (thisCreature.Boredom == 70)
+            {
+                txtOutPut.Text = txtOutPut.Text + "Can we do something?\n";
+            }
+            if (thisCreature.Boredom == 90)
+            {
+                txtOutPut.Text = txtOutPut.Text + "I think I know what will be fun\n";
+            }
+            if(thisCreature.Boredom == 100)
+            {
+                txtOutPut.Text = txtOutPut.Text + "RAAAGGEEEEE!!! " + thisCreature.Name + " Rips your head off and plays vollyball with his friends with your head\n and eats the rest for dinner";
+                btnFeed.Enabled = false;
+                btnListen.Enabled = false;
+                btnPlay.Enabled = false;
+                btnTrick.Enabled = false;
+                barBoardem.Value = barBoardem.Minimum;
+                timer1.Stop();
+            }
+            #endregion
+        }
+
+        private void btnTrick_Click(object sender, EventArgs e)
+        {
+            barBoardem.Increment(boardRate);
+            thisCreature.increaseBoardem(boardRate);
+            txtOutPut.Text = txtOutPut.Text + "This is only fun for you!\n";
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            thisCreature.reduceBoardem(boardRate);
+            barBoardem.Increment(-boardRate);
+            txtOutPut.Text = txtOutPut.Text + "HHeey " + thisCreature.Name + " Look at mee!!\n";
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = "Current Mood: " + thisCreature.getMood(thisCreature.Boredom, thisCreature.Hunger) + "\n";
+        }
+    }
+
+    class Critter
+{
+    int m_hunger = 0, m_boredom = 0;
+    string m_name = "";
+
+    public Critter(string _name, int _hunger, int _boredom)
+    {
+        Boredom = _boredom;
+        Hunger = _hunger;
+        Name = _name;
+    }
+   
+    public string getMood(int board, int hunger)
+    {
+            int moodNumber = (board);
+            string mood = " . . . ";
+
+            if(moodNumber == 20)
+            {
+                mood = "Indifferent";
+            }     
+            if(moodNumber == 30)
+            { 
+                mood = "Not thinking about murder. . . ";
+            }
+
+            if (moodNumber == 40)
+            {
+                mood = "Kind of Angry";
+            }
+            if(moodNumber == 70)
+            {
+                mood = "Will eat half of all children in a 10 mile raduis";
+            }
+            if (moodNumber == 80)
+            {
+                mood = "Feels fine, (obviously lying DO SOMETHING!!)";
+            }
+            if(moodNumber == 100)
+            {
+                mood = "Will paint the town red!";
+            }
+
+            return mood;    
+    }
+    public void reduceBoardem(int boardem)
+    {
+      Boredom -= boardem;
+    }
+    public void increaseBoardem(int boardem)
+    {
+      Boredom += boardem;
+    }
+    public void feed(int foodAmt)
+    {
+      Hunger += foodAmt;
+    }
+
+    public int Boredom
+    {
+        get
+        {
+            return m_boredom;
+        }
+
+        set
+        {
+            m_boredom = value;
+        }
+    }
+
+    public int Hunger
+    {
+        get
+        {
+            return m_hunger;
+        }
+
+        set
+        {
+            m_hunger = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return m_name;
+        }
+
+        set
+        {
+            m_name = value;
+        }
+    }
+}
+}
+
+
